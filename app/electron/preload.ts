d536d418eaf88a88c09cc4a693f624e491735286
yolossn <sannagaraj@microsoft.com>
2024-05-14 01:40:13 +0530

    app: Update desktopApi send method to accept multiple args
    This patch updates the send method to accept
    multiple arguments. This allows for more
    flexibility in sending data via IPC channels
    from renderer process to the main process
    
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

--- app/electron/preload.ts
+++ app/electron/preload.ts
@@ -3,7 +3,7 @@ import { contextBridge, ipcRenderer } from 'electron';
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('desktopApi', {
-  send: (channel, data) => {
+  send: (channel, ...data) => {
     // allowed channels
     const validChannels = [
       'setMenu',
@@ -18,7 +18,7 @@ contextBridge.exposeInMainWorld('desktopApi', {
       'cancel-plugin-process',
     ];
     if (validChannels.includes(channel)) {
-      ipcRenderer.send(channel, data);
+      ipcRenderer.send(channel, ...data);
     }
   },
   receive: (channel, func) => {
